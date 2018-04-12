a9adc7d47e062a1c0d227fdf667ee156a4d37bd7
sfmeyer.btc <stefan.meyer@btc-ag.com>
2018-04-12 13:46:10 +0200

    My first sass npm gulp project

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # sassNpmGulp
+
+wichtige befehle
+
+Aufsetzen des Projektes
+- npm init
+
+Version von MPM
+- npm -v 
+
+Update npm
+- sudo npm install -g npm
+
+installation gulp
+- sudo npm install -g gulp
+
+installation module
+- npm install browser-sync --save-dev
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const autoprefixer = require('gulp-autoprefixer');
+
+const SOURCEPATHS = {
+    sassSource: 'src/scss/*.scss',
+    htmlSource: 'src/*.html'
+};
+
+const APPPATH = {
+    root: 'app',
+    css: 'app/css',
+    js: 'app/js'
+};
+
+gulp.task('sass', function () {
+    return gulp.src(SOURCEPATHS.sassSource)
+            .pipe(autoprefixer('last 10 versions'))
+            .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+            .pipe(gulp.dest(APPPATH.css))
+});
+
+gulp.task('serve', ['sass'], function () {
+    browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
+        server: {
+            baseDir: APPPATH.root
+        }
+    });
+});
+
+gulp.task('copy', function () {
+    gulp.src(SOURCEPATHS.htmlSource)
+            .pipe(gulp.dest(APPPATH.root));
+});
+
+gulp.task('watch', ['serve', 'copy'], function () {
+    gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+    gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
+});
+
+gulp.task('default', ['watch']);
